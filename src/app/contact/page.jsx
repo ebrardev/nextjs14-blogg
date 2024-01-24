de7f822d538a72f5e365e20231e91bf5d1bfa3a5
ebrardev <16155052@mersin.edu.tr>
2024-01-24 14:16:36 +0300

    contact

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,21 @@
-
+import styles from './contact.module.css'
+import Image from 'next/image'
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+<div className={styles.container}>
+  <div className={styles.imgContainer}>
+    <Image src="/contact.png" alt="picture" fill className={styles.img} />
+    </div>
+    <div className={styles.formContainer}>
+      <form action='' className={styles.form}>
+        <input type="text" placeholder="Name Surname" className={styles.input} />
+        <input type="email" placeholder="email adress" className={styles.input} />
+        <input type="text" placeholder="Phone number" className={styles.input} />
+        <textarea placeholder="Message" cols="30" rows="10" className={styles.textarea} />
+        <button className={styles.btn}>Send</button>
+        </form>
+      </div>
+  </div>
   )
 }
 
