45afbcb098068a878cdc47d6ab7cf8080493142e
ebrardev <16155052@mersin.edu.tr>
2024-01-24 13:41:43 +0300

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,36 @@
-
+import styles from './about.module.css'
+import Image from 'next/image'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+<div className={styles.container}>
+  <div className={styles.textContainer}>
+    <h2 className={styles.subtitle}>About Agency</h2>
+    <h1 className={styles.title}>
+      We are a creative studio focusing on culture, luxury, editorial & art. Somewhere between sophistication and simplicity.
+    </h1>
+    <p className={styles.desc}>
+      We are a creative studio focusing on culture, luxury, editorial & art. Somewhere between sophistication and simplicity.e a creative studio focusing on culture, luxury, editorial & art. Somewhere between sophistication and simplicity.
+    </p>
+    <div className={styles.boxes} >
+      <div className={styles.box}>
+        <h1>10 K+</h1>
+        <p>Project completed</p>
+        </div>
+        <div className={styles.box}>
+        <h1>10 K+</h1>
+        <p>Project completed</p>
+        </div>
+        <div className={styles.box}>
+        <h1>10 K+</h1>
+        <p>Project completed</p>
+        </div>
+    </div>
+  </div>
+  <div className={styles.imgContainer}>
+    <Image src="/about.png" alt="picture" fill className={styles.img} />
+  </div>
+  </div>
   )
 }
 
