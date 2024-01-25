ce88fb92d70f1d37abbf41c12b24c0704d0cc0b0
ebrardev <16155052@mersin.edu.tr>
2024-01-26 00:34:22 +0300

    single blog page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './singlePost.module.css'
 
 const SinglePostPage = () => {
   return (
-    <div>SinglePostPage</div>
+    <div className={styles.container}>
+      
+        <div className={styles.imgContainer}>
+          <Image src="/post.png" alt="" fill className={styles.img} />
+        </div>
+     
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Title</h1>
+        <div className={styles.detail}>
+         
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>
+        
+            </span>
+          </div>
+        </div>
+        <div className={styles.content}>kfksjjkfdsjfdfdssjkdfjdkfjdksjk</div>
+      </div>
+    </div>
   )
 }
 
